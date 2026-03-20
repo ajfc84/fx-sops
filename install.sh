8433fd298caf9f06274bf9ae9cd25795825e5df4
Toni Felizardo <fxsofttech@proton.me>
2026-03-20 08:29:55 -0300

    fix cmd

--- install.sh
+++ install.sh
@@ -8,5 +8,6 @@ echo "creating secrets volume..."
 docker volume create secrets
 
 docker_exec "${CI_PROJECT_NAME}" "${REGISTRY_IMAGE}" "${LATEST_VERSION}" \
+    "" \
     "-v secrets:/app/secrets" \
     "-v /home/b01t/.ssh:/root/.ssh:ro"
