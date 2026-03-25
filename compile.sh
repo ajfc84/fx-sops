7ca7bf9aad0c21f9dc6ed2fa709170b998d751bc
Toni Felizardo <fxsofttech@proton.me>
2026-03-25 10:49:06 -0300

    fx-ops sops multi env config

--- compile.sh
+++ compile.sh
@@ -9,4 +9,4 @@ cp_src "build" "src"
 
 echo "copying secrets file from ${CI_ENVIRONMENT_NAME} env to ${CI_PROJECT_NAME} project..."
 cp "${CI_PROJECT_DIR}/secrets.${CI_ENVIRONMENT_NAME}.yml" "${SUB_PROJECT_DIR}/build/secrets.yml"
-cp "${CI_PROJECT_DIR}/.sops.yaml" "${SUB_PROJECT_DIR}/build/"
+cp "${CI_PROJECT_DIR}/.sops.${CI_ENVIRONMENT_NAME}.yaml" "${SUB_PROJECT_DIR}/build/.sops.yaml"
