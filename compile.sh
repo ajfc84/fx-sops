0a3de1cb0dbf179730136526a66e5ac5ffdb5b5d
Toni Felizardo <fxsofttech@proton.me>
2026-03-27 13:15:06 -0300

    compile check

--- compile.sh
+++ compile.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 . "${CI_PROJECT_DIR}/buildSh/utils.sh"
 
 mk_build
@@ -8,5 +7,15 @@ mk_build
 cp_src "build" "src"
 
 echo "copying secrets file from ${CI_ENVIRONMENT_NAME} env to ${CI_PROJECT_NAME} project..."
-cp "${CI_PROJECT_DIR}/secrets.${CI_ENVIRONMENT_NAME}.yml" "${SUB_PROJECT_DIR}/build/secrets.yml"
-cp "${CI_PROJECT_DIR}/.sops.${CI_ENVIRONMENT_NAME}.yaml" "${SUB_PROJECT_DIR}/build/.sops.yaml"
+
+SOPS_SECRETS_FILE="${CI_PROJECT_DIR}/.secrets.${CI_ENVIRONMENT_NAME}.yaml"
+if [ ! -f "${SOPS_SECRETS_FILE}" ]; then
+    SOPS_SECRETS_FILE="${CI_PROJECT_DIR}/.secrets.yaml"
+fi
+cp "${SOPS_SECRETS_FILE}" "${SUB_PROJECT_DIR}/build/secrets.yml"
+
+SOPS_CONFIG_FILE="${CI_PROJECT_DIR}/.sops.${CI_ENVIRONMENT_NAME}.yaml"
+if [ ! -f "${SOPS_CONFIG_FILE}" ]; then
+    SOPS_CONFIG_FILE="${CI_PROJECT_DIR}/.sops.yaml"
+fi
+cp "${SOPS_CONFIG_FILE}" "${SUB_PROJECT_DIR}/build/.sops.yaml"
