1ac604ee5f6f0fdb3151a763582b525d9695a467
Toni Felizardo <fxsofttech@proton.me>
2026-04-27 16:23:14 -0300

    fix dot secrets

--- compile.sh
+++ compile.sh
@@ -8,14 +8,16 @@ cp_src "build" "src"
 
 echo "copying secrets file from ${CI_ENVIRONMENT_NAME} env to ${CI_PROJECT_NAME} project..."
 
-SOPS_SECRETS_FILE="${CI_PROJECT_DIR}/.secrets.${CI_ENVIRONMENT_NAME}.yaml"
-if [ ! -f "${SOPS_SECRETS_FILE}" ]; then
-    SOPS_SECRETS_FILE="${CI_PROJECT_DIR}/.secrets.yaml"
+SOPS_SECRETS_FILE="${CI_PROJECT_DIR}/secrets.${CI_ENVIRONMENT_NAME}.yaml"
+if [ ! -f "${SOPS_SECRETS_FILE}" ];
+then
+    SOPS_SECRETS_FILE="${CI_PROJECT_DIR}/secrets.yaml"
 fi
 cp "${SOPS_SECRETS_FILE}" "${SUB_PROJECT_DIR}/build/secrets.yml"
 
 SOPS_CONFIG_FILE="${CI_PROJECT_DIR}/.sops.${CI_ENVIRONMENT_NAME}.yaml"
-if [ ! -f "${SOPS_CONFIG_FILE}" ]; then
+if [ ! -f "${SOPS_CONFIG_FILE}" ];
+then
     SOPS_CONFIG_FILE="${CI_PROJECT_DIR}/.sops.yaml"
 fi
 cp "${SOPS_CONFIG_FILE}" "${SUB_PROJECT_DIR}/build/.sops.yaml"
